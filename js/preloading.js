59bd11fe057231bcb646210a54e26eef92737943
nethsaradilshan <nethsaradilshan97@gmail.com>
2020-04-28 18:07:20 +0530

    Preloading

---
+++ js/preloading.js
@@ -0,0 +1,18 @@
+(function() {
+
+    var preload = document.getElementById("preload");
+    var loading = 0;
+    var id = setInterval(frame, 40);
+
+    function frame() {
+        if(loading == 100){
+            clearInterval(id);
+            window.open("../pages/Test.html","_self");
+        }else{
+            loading = loading +1;
+            if(loading == 90){
+                preload.style.animation="fadeout is ease";
+            }
+        }
+    }
+})();
